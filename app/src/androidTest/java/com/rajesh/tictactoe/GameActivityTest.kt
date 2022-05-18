d3f97c0a3b0fa44acc1d271e25217dbc674c455f
rajesh-udhayan <rajeshkudhayan@gmail.com>
2022-05-18 23:46:03 +0530

    Composable for Grid created and UI test done for the same

--- app/src/androidTest/java/com/rajesh/tictactoe/GameActivityTest.kt
+++ app/src/androidTest/java/com/rajesh/tictactoe/GameActivityTest.kt
@@ -1,20 +1,80 @@
 package com.rajesh.tictactoe
 
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsSelectable
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.LiveData
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-
-class GameActivityTest{
+class GameActivityTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun shouldDisplayTicTacToeText(){
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             TicTacToeTheme {
-                GameView()
+                GridButtons(
+                    cards = listOf(
+                        listOf(
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 0, 0),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 1, 0),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 2, 0)
+                        ),
+                        listOf(
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 0, 1),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 1, 1),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 2, 1)
+                        ),
+                        listOf(
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 0, 2),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 1, 2),
+                            GridCell(TicTacToe.Game.PLAYER_NONE, 2, 2)
+                        )
+                    )
+                )
             }
+        }
+    }
+
+    @Test
+    fun shouldDisplayTicTacToeText() {
+        with(composeTestRule) {
+            val title = onNodeWithText("TicTacToe")
+
+            title.assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun shouldDisplayGridCells() {
+        with(composeTestRule) {
+            val cellOne = onNodeWithTag("Cell 00")
+            val cellTwo = onNodeWithTag("Cell 01")
+            val cellThree = onNodeWithTag("Cell 02")
+            val cellFour = onNodeWithTag("Cell 10")
+            val cellFive = onNodeWithTag("Cell 11")
+            val cellSix = onNodeWithTag("Cell 12")
+            val cellSeven = onNodeWithTag("Cell 20")
+            val cellEight = onNodeWithTag("Cell 21")
+            val cellNine = onNodeWithTag("Cell 22")
+
+            cellOne.assertIsDisplayed()
+            cellTwo.assertIsDisplayed()
+            cellThree.assertIsDisplayed()
+            cellFour.assertIsDisplayed()
+            cellFive.assertIsDisplayed()
+            cellSix.assertIsDisplayed()
+            cellSeven.assertIsDisplayed()
+            cellEight.assertIsDisplayed()
+            cellNine.assertIsDisplayed()
         }
     }
 }
--- app/src/main/java/com/rajesh/tictactoe/GameActivity.kt
+++ app/src/main/java/com/rajesh/tictactoe/GameActivity.kt
@@ -3,47 +3,123 @@ package com.rajesh.tictactoe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class GameActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<GameViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-            setContent{
-                TicTacToeTheme {
-                    Surface(color = MaterialTheme.colors.background) {
-                        GameView()
+        viewModel.initGridBoxes()
+        setContent {
+            TicTacToeTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    GameView(viewModel)
+                }
+            }
+        }
+    }
+
+}
+
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    fun GameView(viewModel: GameViewModel) {
+        val cards: List<List<GridCell>>by viewModel.getBoxes().observeAsState(listOf())
+        Column {
+            GridButtons(cards){
+                viewModel.selectBox(it)
+            }
+        }
+    }
+
+    @Composable
+    fun GridButtons( cards: List<List<GridCell>>, boxSelected: (card: GridCell) -> Unit = {}) {
+        Text(text = "TicTacToe")
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            cards.forEach { rows ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.White),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    rows.forEach { card ->
+                        ActionButton(card) {
+                            boxSelected(card)
+                        }
                     }
                 }
             }
-    }
-}
-
-@Composable
-fun GameView() {
-    Text(text = "TicTacToe")
-}
-
-@Composable
-fun TicTacToeTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colors = lightColors(),
-        typography = Typography(),
-        shapes = Shapes(),
-        content = content
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview(){
-    TicTacToeTheme {
-        Column {
-            GameView()
         }
     }
-}
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    fun ActionButton(card: GridCell, selectBox: () -> Unit = {}) {
+        val indexTag = "Cell ${card.rowIndex}${card.columnIndex}"
+        Card(
+            modifier = Modifier
+                .padding(all = 10.dp)
+                .border(
+                    width = 2.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(5.dp),
+                )
+                .height(100.dp)
+                .width(100.dp).
+            testTag(indexTag),
+            backgroundColor = Color.White,
+            onClick = selectBox
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = card.showPlayer(),
+                    fontSize = 34.sp,
+                    color = Color.Black,
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun TicTacToeTheme(content: @Composable () -> Unit) {
+        MaterialTheme(
+            colors = lightColors(),
+            typography = Typography(),
+            shapes = Shapes(),
+            content = content
+        )
+    }
